ab4048e2eccce7294856b84321b7259ef3dff321
theohdv <theo@dmbook.pro>
2017-10-11 10:22:49 +0100

    Fix RN > 0.47 compatibility

--- android/src/main/java/com/dmbook/RNOsSettingsManager/RNOsSettingsManagerPackage.java
+++ android/src/main/java/com/dmbook/RNOsSettingsManager/RNOsSettingsManagerPackage.java
@@ -17,7 +17,6 @@ public class RNOsSettingsManagerPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new RNOsSettingsManagerModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
